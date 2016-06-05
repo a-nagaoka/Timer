b4746bf70a6cc444e2c381720e704348ed42ee12
Ayumi Nagaoka <a-nagaoka@esm.co.jp>
2016-06-06 08:38:13 +0900

    タイマー処理をCountdownTimerを使用するよう修正
    - タイマーの時間入力をNumberPickerを使用するよう修正

---
+++ AppTimer/app/src/main/java/com/example/apptimer/DrawView.java
@@ -0,0 +1,13 @@
+package com.example.apptimer;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by a-nagaoka on 2016/06/05.
+ */
+public class DrawView extends View{
+    public DrawView(Context context) {
+        super(context);
+    }
+}
--- AppTimer/app/src/main/java/com/example/apptimer/MainActivity.java
+++ AppTimer/app/src/main/java/com/example/apptimer/MainActivity.java
@@ -1,43 +1,198 @@
 package com.example.apptimer;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.format.Formatter;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
+    // メインアクティビティ
     private MainActivity self = this;
+    // タイマー
+    private AppCountDownTimer mTimer;
+    // スタートボタン
+    private View mStartButton;
+    // ストップボタン
+    private View mStopButton;
+    // ステータス
+    private TextView mStatusMessage;
+    // 時間ピッカー
+    private NumberPicker mTimePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener(){
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // ボタン・テキストの初期化
+        initialize();
+
+        // TODO アニメーションの初期化
+        ViewAnimator viewAnimator = (ViewAnimator) findViewById(R.id.viewAnimator);
+
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+
+        if (mTimer != null) {
+            mTimer.cancel();
+        }
+    }
+
+    /**
+     * 初期化します。
+     */
+    private void initialize() {
+        mStartButton = findViewById(R.id.startButton);
+        mStopButton = findViewById(R.id.stopButton);
+        mStatusMessage = (TextView) findViewById(R.id.appTimerStatus);
+        mTimePicker = (NumberPicker) findViewById(R.id.numberPicker);
+
+        // 初期化
+        setNumberPicker(mTimePicker, 5, 60, 5, R.string.timeFormat);
+        // 初期値は20分に設定
+        mTimePicker.setValue(3);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // ボタン更新
-                findViewById(R.id.startButton).setEnabled(false);
-                findViewById(R.id.stopButton).setEnabled(true);
-                // 入力時間
-                String text = ((EditText)findViewById(R.id.editText)).getText().toString();
-                int time = Integer.parseInt(text);
-                // タイマー開始
-                Intent intent = new Intent(self, TimerIntentService.class);
-                intent.putExtra("WAIT_TIME", time);
-                startService(intent);
+                // 入力時間取得
+                //String text = ((EditText) findViewById(R.id.editText)).getText().toString();
+                //int time = Integer.parseInt(text);
+                int time = (mTimePicker.getValue() + 1) * 5;
 
+                // タイマー開始
+                mTimer = new AppCountDownTimer(time * 60 * 1000, 60 * 1000);
+                mTimer.start();
+
+                // メニュー画面に戻す
                 moveTaskToBack(true);
 
-                // TODO トースト表示
+                // トースト表示
                 String message = getString(R.string.messageStarted, time);
                 Toast.makeText(self, message, Toast.LENGTH_LONG).show();
+
+                // ボタン更新
+                mStartButton.setEnabled(false);
+                mStopButton.setEnabled(true);
+                mTimePicker.setEnabled(false);
+
+                // メッセージ更新
+                mStatusMessage.setText(getResources().getString(R.string.statusStarted, time));
             }
         });
+
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // タイマー停止
+                mTimer.cancel();
+
+                // タイマーの状態を更新
+                stopTimer();
+            }
+        });
+    }
+
+    /**
+     * 数字ピッカーを初期化します。
+     * @param picker
+     * @param min
+     * @param max
+     * @param step
+     * @param format
+     */
+    private void setNumberPicker(NumberPicker picker, int min, int max, int step, int format) {
+        picker.setMinValue(min / step - 1);
+        picker.setMaxValue((max / step) - 1);
+        String[] valueSet = new String[max / min];
+        for (int i = min; i <= max; i += step) {
+            valueSet[(i / step) - 1] = getString(format, i);
+        }
+        picker.setDisplayedValues(valueSet);
+    }
+
+    /**
+     * タイマーの状態を更新します。
+     */
+    private void stopTimer() {
+        // タイマーが動いている場合はキャンセル
+        if (mTimer != null) {
+            mTimer.cancel();
+            mTimer = null;
+        }
+
+        // ボタン更新
+        mStartButton.setEnabled(true);
+        mStopButton.setEnabled(false);
+
+        // 時間選択ピッカーを有効に設定
+        mTimePicker.setEnabled(true);
+
+        // メッセージ更新
+        mStatusMessage.setText(getResources().getString(R.string.statusStopped));
+    }
+
+    /**
+     * カウントダウンタイマー
+     */
+    public class AppCountDownTimer extends CountDownTimer {
+
+        /**
+         * @param millisInFuture    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param countDownInterval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        public AppCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long time = millisUntilFinished / (60 * 1000);
+            // メッセージ更新
+            mStatusMessage.setText(getResources().getString(R.string.messageStarted, time));
+        }
+
+        @Override
+        public void onFinish() {
+            // タイマーの状態を更新
+            mTimer = null;
+            stopTimer();
+
+            // 終了のお知らせ画面に遷移
+            Intent alertIntent = new Intent(self, NoticeActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(self, 0, alertIntent, 0);
+
+            try {
+                pendingIntent.send();
+            } catch (PendingIntent.CanceledException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
