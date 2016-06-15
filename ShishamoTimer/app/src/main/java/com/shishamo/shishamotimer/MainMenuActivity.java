b8f1e71c727fff9ca98d38521cc5c74a46b1d42a
Ayumi Nagaoka <a-nagaoka@esm.co.jp>
2016-06-16 05:39:40 +0900

    ししゃもタイマーのベースを作成

---
+++ ShishamoTimer/app/src/main/java/com/shishamo/shishamotimer/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package com.shishamo.shishamotimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
